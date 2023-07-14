a41810918ecdc6dae32e628691fc7e615ac1d19d
gaoyang <xz@lgysh.cn>
2023-07-14 18:57:53 +0800

    remove execFile

--- bin/gen-grpc-web/main.js
+++ bin/gen-grpc-web/main.js
@@ -6,24 +6,26 @@ const cp = require('child_process')
 
 module.exports = function genGrpcWeb(args, stdout, stderr) {
   try {
-    const protoDir = args[0]
-    const outDir = args[1]
+    const protoDir = path.resolve(process.cwd(), args[0])
+    const outDir = path.resolve(process.cwd(), args[1])
 
     if (fs.existsSync(outDir)) {
       fs.rmSync(outDir, { recursive: true, force: true })
     }
     fs.mkdirSync(outDir, { recursive: true })
 
-    var protoc = path.resolve(__dirname, '../../node_modules/.bin/grpc_tools_node_protoc')
-    var plugin = path.resolve(__dirname, '../../node_modules/.bin/protoc-gen-ts_proto')
+    const protoc = path.resolve(__dirname, '../../node_modules/.bin/grpc_tools_node_protoc')
+    const plugin = path.resolve(__dirname, '../../node_modules/.bin/protoc-gen-ts_proto')
 
-    var args = ['--plugin=protoc-gen-ts_proto=' + plugin].concat(args.slice(3))
-
-    args.push(`--ts_proto_out=${outDir}`)
-    args.push(`-I=${protoDir} ${protoDir}/*.proto`)
+    const command = []
+    command.push(protoc)
+    command.push(`--plugin=protoc-gen-ts_proto=${plugin}`)
+    command.push(`--ts_proto_out=${outDir}`)
+    command.push(`--ts_proto_opt=outputClientImpl=grpc-web`)
+    command.push(`-I=${protoDir} ${protoDir}/*.proto`)
 
     const promise = new Promise((resolve, reject) => {
-      var child_process = cp.execFile(protoc, args, function (error, stdout, stderr) {
+      const child_process = cp.exec(command.join(' '), (error, stdout, stderr) => {
         if (error) {
           return reject(error)
         }
@@ -32,10 +34,6 @@ module.exports = function genGrpcWeb(args, stdout, stderr) {
 
       child_process.stdout.pipe(stdout)
       child_process.stderr.pipe(stderr)
-    })
-
-    promise.catch(err => {
-      console.error('ERROR:', err.message)
     })
 
     return promise
