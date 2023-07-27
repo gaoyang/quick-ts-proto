1fee0f6e2425dd0c0990be3348295dc17a76c73c
gaoyang <xz@lgysh.cn>
2023-07-27 10:44:01 +0800

    add html homepage.

--- tests/grpc-web/src/index.ts
+++ tests/grpc-web/src/index.ts
@@ -1,7 +1,7 @@
 import { ExampleGrpcServiceClientImpl, GrpcWebImpl } from './grpc/example'
 
-const rpc = new GrpcWebImpl('http://localhost:9090', {})
+const rpc = new GrpcWebImpl('http://localhost:5555', {})
 const client = new ExampleGrpcServiceClientImpl(rpc)
 client.SubscribeUserInfoChanged({}).subscribe(response => {
-  console.log(response.message)
+  document.body.innerText += response.message
 })
